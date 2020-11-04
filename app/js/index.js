8aa2a1991a1ad3703e74619af283227374b1c86e
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-11-04 21:32:31 +0100

    button done

--- app/js/index.js
+++ app/js/index.js
@@ -4,3 +4,33 @@ var tooltipTriggerList = [].slice.call(
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+const hamburger = document.querySelector(
+  ".header .nav-bar .nav-list .hamburger"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#485563";
+  } else {
+    header.style.backgroundColor = "#254441";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
