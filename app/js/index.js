7a6e276deb1ac4758918223fef25c87c669ed3e3
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-11-06 14:06:02 +0100

    deleat js 1 to 6

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,9 @@
-var tooltipTriggerList = [].slice.call(
+/*var tooltipTriggerList = [].slice.call(
   document.querySelectorAll('[data-toggle="tooltip"]')
 );
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
-});
+});*/
 
 const hamburger = document.querySelector(
   ".header .nav-bar .nav-list .hamburger"
@@ -33,4 +33,58 @@ menu_item.forEach((item) => {
     hamburger.classList.toggle("active");
     mobile_menu.classList.toggle("active");
   });
+});
+
+let inputName = document.getElementById("input-name");
+let inputeEmail = document.getElementById("input-email");
+let inputTextarea = document.getElementById("input-textarea");
+let inputSubmit = document.getElementById("input-submit");
+
+console.log(inputSubmit.value);
+
+inputSubmit.addEventListener("click", function (e) {
+  if (inputName.value == "") {
+    inputName.style.borderColor = "red";
+    console.log("Name is empty");
+  }
+  if (inputeEmail.value == "") {
+    inputeEmail.style.borderColor = "red";
+    console.log("Email is empty");
+  }
+  if (inputTextarea.value == "") {
+    inputTextarea.style.borderColor = "red";
+    console.log("Textarea is empty");
+  }
+  if (
+    inputName.value == "" &&
+    inputeEmail.value == "" &&
+    inputTextarea.value == ""
+  ) {
+    inputName.style.borderColor = "red";
+    inputeEmail.style.borderColor = "red";
+    inputTextarea.style.borderColor = "red";
+    console.log("Name Email Message are empty");
+  }
+  if (inputName.value.length > 0) {
+    inputName.style.borderColor = "black";
+  }
+  if (inputeEmail.value.length > 0) {
+    inputeEmail.style.borderColor = "black";
+  }
+  if (inputTextarea.value == "") {
+    inputTextarea.style.borderColor = "black";
+    console.log("Textarea is empty");
+  } else if (inputName.value == "") {
+    inputName.style.borderColor = "black";
+    inputName.style.borderColor = "red";
+    console.log("Name is empty");
+  } else if (inputeEmail.value == "") {
+    inputeEmail.style.borderColor = "black";
+    inputeEmail.style.borderColor = "red";
+    console.log("Email is empty");
+  } else if (inputTextarea.value == "") {
+    inputTextarea.style.borderColor = "black";
+    inputTextarea.style.borderColor = "red";
+    console.log("Textareais empty");
+  }
 });
